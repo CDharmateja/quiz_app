1639570d912f851b17a058f706265d5add00e741
CDharmateja <dharmateja03@yahoo.com>
2018-01-25 19:12:32 +0530

    questions

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var state = {
     items : [{ques: 'If there are three apples and you took two away, how many do you have?', options: ['one', 'two', 'three', 'none'], ans: "two"},
-            {ques: 'Which is heavier, 100 pounds of rocks or 100 pounds of gold?', options: ['100 pounds of gold', '100 pounds of rock', 'They weigh the same'], ans: "They weigh the same"},
+            {ques: 'Which is heavier, 100 pounds of rocks or 100 pounds of gold?', options: ['100 pounds of gold', '100 pounds of rock', 'They weigh the same', 'Can\'t say'], ans: "They weigh the same"},
             {ques: 'How many sides does a circle have?', options: ["None, It's a circle", "The back", "Four", "Two"], ans: "Two"},
-            {ques: 'If you pass the 2nd person in a race what will You be coming?', options: ['3rd', '1st', '2nd'], ans: "2nd"}]
+            {ques: 'If you pass the 2nd person in a race what will You be coming?', options: ['3rd', '1st', '2nd', 'information is insufficent'], ans: "2nd"},
+            {ques: 'Mary\'s father had 5 children:\
+            Mimi\
+            Mumu\
+            Mama\
+            Meme\
+            What was the 5th child\'s name?', options: ['Momo', 'Mary', 'Marie', 'None'], ans: "Mary"},
+            {ques: 'What is light as a feather, but even the strongest man cannot hold it more than a few minutes?', options: ['A feather', 'A smile', 'His breath', 'None'], ans: 'His breath'},
+            {ques: 'There was an airplane crash, every single person died, but two people survived. How is this possible?', options: ['They were not on the airplane', 'They were married', 'The 2 were saved by others', 'None'], ans: 'They were married'},
+            {ques: 'How far can you walk into the woods?', options: ['Halfway', 'All the way and then back again', 'As far as you want', 'None'], ans: 'Halfway'},
+            {ques: 'The Mississippi River is the dividing line between Tennessee and Arkansas. If an airplane crashed exactly in the middle of the Mississippi River there, where would the survivors be buried?', options: ['In the town of their birth', 'Nowhere, you don\'t bury survivors', 'Whatever side the people were on', 'None'], ans: "you don't bury survivors"}]
 };
 
 var render = function() {
@@ -35,15 +45,33 @@ var renderQuiz = function(i = 0) {
             <div class="inline">' + state.items[i].options[3] + '</div>\
         </div>\
         <nav>\
-            <button>next</button>\
+            <button class="js-next">next</button>\
         </nav>\
     </div>')
 };
+
+var nextQues = function(quesNo) {
+    $('.js-next').on('click', function() {
+        renderQuiz(quesNo);
+        optionSelect();
+        quesNo++;
+        nextQues(quesNo);
+    });
+};
+
+var optionSelect = function() {
+    $('input[type="radio"]').on('change', function() {
+        $('input[type="radio"]').not(this).prop('checked', false);
+    });
+}
+
 
 $(function() {
     var quesNo = 0;
     $('.js-start').on('click', function() {
         renderQuiz(quesNo);
+        optionSelect();
         quesNo += 1;
+        nextQues(quesNo);
     });
 });
